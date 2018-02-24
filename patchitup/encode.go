7cb719dd556d7a82121b99e40c943bbf23e8914b
Zack Scholl <zack.scholl@gmail.com>
2018-02-24 10:18:32 -0800

    Add encode

---
+++ patchitup/encode.go
@@ -0,0 +1,50 @@
+packate patchitup 
+
+
+func encode(s string) (encoded string) {
+	// compress patch
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	if _, err := gz.Write([]byte(s)); err != nil {
+		panic(err)
+	}
+	if err := gz.Flush(); err != nil {
+		panic(err)
+	}
+	if err := gz.Close(); err != nil {
+		panic(err)
+	}
+
+	// encrypt patch
+	encrypted := utils.Encrypt(b.Bytes(), []byte(`1234`))
+
+	// convert to base64
+	encoded = base64.StdEncoding.EncodeToString(encrypted)
+
+	log.Debugf("compressed patch from %s to %s", humanize.Bytes(uint64(len(s))), humanize.Bytes(uint64(len(encoded))))
+	return
+}
+
+func decode(s string) (decoded string, err error) {
+	// convert from base64
+	patchBytes, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return
+	}
+
+	// decrypt
+	decrypted, err := utils.Decrypt(patchBytes, []byte(`1234`))
+	if err != nil {
+		return
+	}
+
+	// decompress
+	gr, err := gzip.NewReader(bytes.NewBuffer(decrypted))
+	defer gr.Close()
+	data, err := ioutil.ReadAll(gr)
+	if err != nil {
+		return
+	}
+	decoded = string(data)
+	return
+}
